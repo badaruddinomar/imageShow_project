b7450108cacee6c7967daa73e4fc1c5d1087fbe3
Badar <badaruddinomar2403@gmail.com>
2022-12-05 18:47:20 +0800

    bg changed

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let bigImg = document.querySelector(`.big-img`);
-let smallImg = document.querySelectorAll(`.small-img`);
+const bigImg = document.querySelector(`.big-img`);
+const smallImg = document.querySelectorAll(`.small-img`);
+const container = document.querySelector(`.container`);
 
-smallImg.forEach((sImgElm) => {
-  sImgElm.addEventListener(`click`, () => {
-    bigImg.attributes.src.textContent = sImgElm.attributes.src.textContent;
-    console.log(`clicked`);
+container.addEventListener(`click`, function (e) {
+  const clicked = e.target.closest(`.small-img`);
+  if (!clicked) return;
+  smallImg.forEach(function (img) {
+    img.classList.remove(`active`);
   });
+  const srcImg = (bigImg.attributes.src.textContent =
+    clicked.attributes.src.textContent);
+  if (srcImg) {
+    clicked.classList.add(`active`);
+  }
 });
